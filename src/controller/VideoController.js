653cfd2dc3177b03248f9b4b5408b7724a34a92b
Elian07 <elians.2016@alunos.utfpr.edu.br>
2019-11-22 09:01:22 -0300

    Fazendo mecanismo de pesquisa

--- src/controller/VideoController.js
+++ src/controller/VideoController.js
@@ -8,13 +8,13 @@ module.exports = {
 
         return res.json(videos)
     },
-    
+/*
     async search(req, res){
-        const pesquisa = req.$search
+        const pesquisa = req.params.q
         const video = await Video.find( { $text: { $search: `${pesquisa}` } } )
         return res.json(video)
     },
-
+*/
     async store(req, res){
         const { titulo, descricao, hashtags} = req.body
         const { filename: video} = req.file
