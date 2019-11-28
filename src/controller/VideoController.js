b1623943534a8f5368a7f1ec4da1de7039882ed3
Elian07 <elians.2016@alunos.utfpr.edu.br>
2019-11-28 19:33:20 -0300

    Arrumando mecanismo de pesquisa

--- src/controller/VideoController.js
+++ src/controller/VideoController.js
@@ -7,17 +7,16 @@ module.exports = {
     async index(req, res, next){
         const videos = await Video.find().sort('-createdAt')
 
-        return res.status(200).json(videos)
+        return res.json(videos)
     },
 
     async search(req, res, next){
         const pesquisa = req.params.value
         const video = await Video.find( { descricao:  { $regex: pesquisa, $options: "si" }  } )
         
-        if(erq.params.value === video){
-            return res.status(200).json(video)
-        }
-        return res.status(404).json("Video não encontrado!")
+        
+            return res.json(video)
+      
     },
     
     async store(req, res, next){
@@ -35,11 +34,11 @@ module.exports = {
             video: nomeArquivo
         })
 
-        if(content.id){
+        
             req.io.emit('video', videos)
-            return res.status(201).json(videos)
-        }
-        return res.status(400).json("video não adicionado")
+            return res.json(videos)
+        
+       
 
         
     }
